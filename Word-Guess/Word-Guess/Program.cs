ce501c2ec3f12755f6919f5f1ee9b32b14e1de41
psingh1028 <pawanjotsingh1997@gmail.com>
2020-09-08 13:26:31 -0400

    Base Project

---
+++ Word-Guess/Word-Guess/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Word Guess app!");
+
+            var phrases = getPhrase();
+
+
+            Console.ReadKey();
+        }
+
+        public static List<String> getPhrase()
+        {
+            var phrases = new List<String>();
+
+            phrases.Add("Durant");
+            phrases.Add("James");
+            phrases.Add("Harden");
+            phrases.Add("Curry");
+            phrases.Add("Irving");
+            phrases.Add("Davis");
+
+            return phrases;
+        }
+    }
+}
